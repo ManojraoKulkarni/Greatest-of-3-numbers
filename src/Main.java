b90cfab02a635819422d9fc44cc3f5dd5735ee3b
Manoj <kulkarnimanojrao@gmail.com>
2025-05-03 23:06:57 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("good night");
+        Scanner sc = new Scanner(System.in);
+        //taking input for variable a from the user
+        System.out.println("Enter the value of a : ");
+        int a = sc.nextInt();
+
+        //taking input for variable b from the user
+        System.out.println("Enter the value of b : ");
+        int b = sc.nextInt();
+
+        //taking input for variable cfrom the user
+        System.out.println("Enter the value of c : ");
+        int c = sc.nextInt();
+
+                // checking a is greater than b
+                if (a > b) {
+                    System.out.println(" a is grater than b");
+
+                    // checking a is greater than c
+                    if (a > c) {
+                        System.out.println("a is greater than c");
+                        System.out.println("a is the greater " + a);
+                    } else {
+                        System.out.println("c is greater than a");
+                        System.out.println("c is the greater " + c);
+                    }
+                }else{
+                    // checking b is greater than c
+                        if(b > c) {
+                            System.out.println("b is the grater");
+                            System.out.println("b is the greater "+b);
+                        }
+                        else{
+                            System.out.println("c is the greater");
+                            System.out.println("c is the greater "+c);
+                }
+            }
+        }
     }
-}
